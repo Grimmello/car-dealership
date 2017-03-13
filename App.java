1b15b0a2a80f35091fe08deede444ed0421df6f3
Clifford Grimmell and Taylor Loftis-Kim <clifford@>
2017-03-13 14:11:06 -0700

    add format to output table, create constructor

--- App.java
+++ App.java
@@ -1,52 +1,34 @@
 import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
 
-    Vehicle hatchback = new Vehicle();
-    hatchback.mYear = 1996;
-    hatchback.mBrand = "Honda";
-    hatchback.mModel = "Civic";
-    hatchback.mMiles = 180000;
-    hatchback.mPrice = 2000;
+    Vehicle hatchback = new Vehicle(1994, "Subaru", "Legacy", 170000, 4000);
+    Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000, 7000);
+    Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
+    Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
+    Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 2000000, 3500);
 
-    Vehicle suv = new Vehicle();
-    suv.mYear = 2010;
-    suv.mBrand = "GMC";
-    suv.mModel = "Yukon";
-    suv.mMiles = 20000;
-    suv.mPrice = 24000;
 
-    Vehicle sedan = new Vehicle();
-    sedan.mYear = 2012;
-    sedan.mBrand = "BMW";
-    sedan.mModel = "M3";
-    sedan.mMiles = 80000;
-    sedan.mPrice = 50000;
-
-    Vehicle truck = new Vehicle();
-    truck.mYear = 2004;
-    truck.mBrand = "Nissan";
-    truck.mModel = "Frontier";
-    truck.mMiles = 100000;
-    truck.mPrice = 6000;
-
-    Vehicle[] allVehicles = {hatchback, suv, sedan, truck};
+    Vehicle[] allVehicles = {hatchback, suv, sedan, truck, crossover};
 
     System.out.println("What is your max budget");
     String stringMaxBudget = myConsole.readLine();
     int MaxBudget = Integer.parseInt(stringMaxBudget);
-
+    System.out.println("-----------------------------");
     System.out.println("Vehicles in your price range:");
+
     for ( Vehicle individualVehicle : allVehicles ) {
       if (individualVehicle.worthBuying(MaxBudget)) {
-        System.out.println("----------------------");
-        System.out.println( individualVehicle.mYear );
-        System.out.println( individualVehicle.mBrand );
-        System.out.println( individualVehicle.mModel );
-        System.out.println( individualVehicle.mMiles );
-        System.out.println( individualVehicle.mPrice );
+        System.out.println("-----------------------------");
+        System.out.println("Year : " + individualVehicle.mYear );
+        System.out.println("Brand: " + individualVehicle.mBrand );
+        System.out.println("Model: " + individualVehicle.mModel );
+        System.out.println("Miles: " + individualVehicle.mMiles );
+        System.out.println("Price: $" + individualVehicle.mPrice );
       }
     }
+    System.out.println("-----------------------------");
   }
 }
--- Vehicle.java
+++ Vehicle.java
@@ -4,7 +4,13 @@ class Vehicle {
   public String mModel;
   public int mMiles;
   public int mPrice;
-
+  public Vehicle(int year, String brand, String model, int miles, int price) {
+    mYear = year;
+    mBrand = brand;
+    mModel = model;
+    mMiles = miles;
+    mPrice = price;
+  }
   public boolean worthBuying(int maxPrice){
     return (mPrice < maxPrice);
   }
