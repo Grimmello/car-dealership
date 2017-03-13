d5921a9f8947b76503853662cefc439bcd8626c5
Clifford Grimmell and Taylor Loftis-Kim <taylor@>
2017-03-13 15:23:57 -0700

    Create while loop for detecting user input.

--- App.java
+++ App.java
@@ -1,4 +1,6 @@
 import java.io.Console;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -8,42 +10,77 @@ public class App {
     Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000, 7000);
     Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
     Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
-    Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 2000000, 3500);
+    Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 200000, 3500);
 
-    Vehicle[] allVehicles = {hatchback, suv, sedan, truck, crossover};
-    System.out.println("-----------------------------");
-    System.out.println("Welcome to our car dealership. What would you like to do?" + "\n" + "Enter one of the following options: All Vehicles or Search Price");
+    List <Vehicle> allVehicles = new ArrayList<Vehicle>();
+    allVehicles.add(hatchback);
+    allVehicles.add(suv);
+    allVehicles.add(sedan);
+    allVehicles.add(truck);
+    allVehicles.add(crossover);
+    System.out.println("---------------------------------------------------------");
+    System.out.println("Welcome to our car dealership. What would you like to do?");
 
-    String navigationChoice = myConsole.readLine();
-    if (navigationChoice.equals("All Vehicles")) {
-      for ( Vehicle individualVehicle : allVehicles ) {
-        System.out.println("-----------------------------");
-        System.out.println("Year : " + individualVehicle.mYear );
-        System.out.println("Brand: " + individualVehicle.mBrand );
-        System.out.println("Model: " + individualVehicle.mModel );
-        System.out.println("Miles: " + individualVehicle.mMiles );
-        System.out.println("Price: $" + individualVehicle.mPrice );
-      }
-    } else if (navigationChoice.equals("Search Price")) {
-      System.out.println("What is your max budget?");
-      String stringMaxBudget = myConsole.readLine();
-      int MaxBudget = Integer.parseInt(stringMaxBudget);
-      System.out.println("-----------------------------");
-      System.out.println("Vehicles in your price range:");
-      for ( Vehicle individualVehicle : allVehicles ) {
-        if (individualVehicle.worthBuying(MaxBudget)) {
+    boolean programRunning = true;
+
+    while(programRunning) {
+      System.out.println("---------------------------------------------------------");
+      System.out.println("Enter one of the following options:"+ "\n"+ "All Vehicles, Search Price, Add Vehicle, Exit");
+      System.out.println("---------------------------------------------------------");
+
+      String navigationChoice = myConsole.readLine();
+      if (navigationChoice.equals("All Vehicles")) {
+        for ( Vehicle individualVehicle : allVehicles ) {
           System.out.println("-----------------------------");
           System.out.println("Year : " + individualVehicle.mYear );
           System.out.println("Brand: " + individualVehicle.mBrand );
           System.out.println("Model: " + individualVehicle.mModel );
           System.out.println("Miles: " + individualVehicle.mMiles );
           System.out.println("Price: $" + individualVehicle.mPrice );
-          }
         }
-      } else {
+      } else if (navigationChoice.equals("Search Price")) {
+        System.out.println("What is your max budget?");
+        String stringMaxBudget = myConsole.readLine();
+        int MaxBudget = Integer.parseInt(stringMaxBudget);
         System.out.println("-----------------------------");
-        System.out.println("I'm sorry, we don't recognize your input.");
+        System.out.println("Vehicles in your price range:");
+        for ( Vehicle individualVehicle : allVehicles ) {
+          if (individualVehicle.worthBuying(MaxBudget)) {
+            System.out.println("-----------------------------");
+            System.out.println("Year : " + individualVehicle.mYear );
+            System.out.println("Brand: " + individualVehicle.mBrand );
+            System.out.println("Model: " + individualVehicle.mModel );
+            System.out.println("Miles: " + individualVehicle.mMiles );
+            System.out.println("Price: $" + individualVehicle.mPrice );
+            }
+          }
+        } else if (navigationChoice.equals("Add Vehicle")){
+            System.out.println("Alright, let's add a vehicle! What year was this vehicle made?");
+            int userVehicleYear = Integer.parseInt(myConsole.readLine());
+            System.out.println("Great! What make or brand is the vehicle?");
+            String userVehicleBrand = myConsole.readLine();
+            System.out.println("Got it! What model is it?");
+            String userVehicleModel = myConsole.readLine();
+            System.out.println("And how many miles does it have on it?");
+            int userVehicleMiles = Integer.parseInt(myConsole.readLine());
+            System.out.println("Finally, what's its price?");
+            int userVehiclePrice = Integer.parseInt(myConsole.readLine());
+            Vehicle userVehicle = new Vehicle(userVehicleYear, userVehicleBrand, userVehicleModel, userVehicleMiles, userVehiclePrice);
+            allVehicles.add(userVehicle);
+            System.out.println("Alright, here's your new vehicle:");
+            System.out.println( "----------------------" );
+            System.out.println( userVehicle.mYear );
+            System.out.println( userVehicle.mBrand );
+            System.out.println( userVehicle.mModel );
+            System.out.println( userVehicle.mMiles );
+            System.out.println( userVehicle.mPrice );
+        } else if (navigationChoice.equals("Exit")) {
+          programRunning = false;
+        } else {
+          System.out.println("-----------------------------");
+          System.out.println("I'm sorry, we don't recognize your input");
+      }
+      System.out.println("-----------------------------");
     }
-    System.out.println("-----------------------------");
   }
 }
